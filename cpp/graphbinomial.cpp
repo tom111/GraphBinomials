dbcd228d672d7fd4fc9af50adac17a964b3b8e68
Thomas Kahle <tomka@gentoo.org>
2012-04-20 11:42:56 -0400

    generate Neighbours

--- cpp/graphbinomial.cpp
+++ cpp/graphbinomial.cpp
@@ -5,47 +5,53 @@
 #include <iostream>
 #include <vector>
 
-vector<Monomial*> generateNeighbours (const vector<Monomial>& base, const vector<Binomial>& steps) {
-  // Warning: allocates memory for the individual monomials
-  // TODO: Implement me
+vector<Monomial*>* generateNeighbours (const vector<Monomial>& base, const vector<Binomial>& steps) {
+  // This function calls functions which allocate memory for new monomials.
+  vector<Monomial*>* result = new vector<Monomial*>;
+  for (int i=0; i<steps.size(); i++){
+    try {
+      result->pushback(base.applyBinomialSafe(steps[i]));
+    }
+    catch (const char* e) {} // Move was not applicable
+    // Try the other direction
+    try {
+      result->pushback(base.applyBinomialSafeReverse(steps[i]))
+    }
+    catch (const char* e) {}  // Move was not applicable
+  };
+  return result;
 }
 
 bool inSameComponent (const Monomial& m1, const Monomial& m2, const vector<Binomial>& moves) {
   // Use breadth first search on the graph.
 
-  // Remember: Vectors are like good old C arrays.  They store pointers!
+  // Remember: Vectors are like good old C arrays.  Pointer arithmetic works!
+  // Our vectors will store pointers
   
-  // A vector to store the known monomials
+  // A vector to store the known monomials.
+  // This will be cleaned up just before returning
   vector<Monomial*> knownMonomials;
   knownMonomials.push_back(new Monomial (m1));
   
   // Previously unseen monomials in the neighbourhood of the current layer
   // Those never need to be cleaned up, they also exist in knownMonomials
   vector<Monomial*> newMonomials;
-  newMonomials.push_back(&m1); // Bootstrap the neighbour generation.  m1 will
-			       // not be deleted.
+  newMonomials.push_back(&m1); // Bootstrap neighbour generation with m1.
 
-  // This will hold new neighbours in each step
   while (newMonomials.size() > 0) {
-    vector<Monomial*> newNeighbours = generateNeighbours (newMonomials, moves);
-    // Everything in newNeighbours is possessed by this function.  Next we
-    // clean the vector newMonomials.  For this we don't need to free anything
-    // since its content is either external (in the first round) or duplicate
-    // (with 'knownMonomials' in all other rounds.)
+    // The vector newNeigbours holds the neighbours of 'newMonomials' in each
+    // step. Everything in newNeighbours is possessed by this while loop.
+    vector<Monomial*>* newNeighbours = generateNeighbours (newMonomials, moves);
+    // newMonomials is not needed anymore now.
     newMonomials.clear();
-    for (int i=0; i<newNeighbours.size(); i++) {
-      Monomial *n = newNeighbours[i];
+    for (int i=0; i<newNeighbours->size(); i++) {
+      Monomial *n = (*newNeighbours)[i];
       if (n->isSame(m1)) {
 	// We are done.  Clean up and return true
 	// Delete the known monomials, we created all of those.
-	for (int j=0; j<knownMonomials.size(); j++){
-	  delete knownMonomials[j]
-	}
+	for (int j=0; j<knownMonomials.size(); j++){ delete knownMonomials[j]; };
 	// All the newNeighbours are still around: Clean them up
-	for (int j=0; j<newNeighbours.size(); j++){
-	  delete newNeighbours[j]
-	}
-	// This should be all
+	for (int j=0; j<newNeighbours->size(); j++){ delete newNeighbours[j]; };
 	return true;
       };
       if (!isPresent (knownMonomials, n)) {
@@ -53,12 +59,14 @@ bool inSameComponent (const Monomial& m1, const Monomial& m2, const vector<Binom
 	knownMonomials.push_back(n);
       }
       else {
-	// we already had n, get rid of it
-	delete newNeighbours[i];
+	// n was already known to lie in the connected component:
+	delete (*newNeighbours)[i];
 	// Now 'n' is invalid, but that should not hurt since the for loop
-	// moves on now.
+	// moves on and will not touch it anymore.
       }
     }; // End of the for loop on newNeighbours
+    // Need to free the vector itself
+    delete newNeighbours;
 
     // Did we find new Monomials?  Generate informative output?
     if (newMonomials.size() > 0) {
@@ -66,7 +74,12 @@ bool inSameComponent (const Monomial& m1, const Monomial& m2, const vector<Binom
     };
   }; // while loop on newMonomials
 
-  // Did not find a connection.  Clean up and go home
+  // Did not find a connection.  Clean up and return.
+
+  // The newMonomials are all either added to the knownMonomials or cleaned up
+  // once we know they are duplicate.  Therefore we only need to clean up the
+  // representation of the connected component.
+  for (int j=0; j<knownMonomials.size(); j++) { delete knownMonomials[j]; };
   
   return false;
 };
@@ -74,5 +87,3 @@ bool inSameComponent (const Monomial& m1, const Monomial& m2, const vector<Binom
 void printMessage (){
   std::cout << "Hallo Cython" << std::endl;
 };
-
-
