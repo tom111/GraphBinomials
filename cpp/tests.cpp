55bcd7186013b073112141e9039323588964bcf4
Thomas Kahle <tomka@gentoo.org>
2012-04-27 17:01:59 +0200

    compile tests

--- cpp/tests.cpp
+++ cpp/tests.cpp
@@ -10,12 +10,12 @@
 
 using namespace std;
 
-int main(){
-  // The simplest example: <x^2 - xy, xy - y^2>
+void t1 () {
+    // The simplest example: <x^2 - xy, xy - y^2>
   // TODO: Figure out smarter way to assing vectors
-  const vector<int> e1; 
-  const vector<int> e2; 
-  const vector<int> e3;
+  vector<int> e1; 
+  vector<int> e2; 
+  vector<int> e3;
   e1.push_back(2);
   e2.push_back(1);
   e3.push_back(0);
@@ -23,18 +23,21 @@ int main(){
   e2.push_back(1);
   e3.push_back(2);
   
-  const Monomial m1(e1);
-  const Monomial m2(e2);
-  const Monomial m3(e3);
+  Monomial m1(e1);
+  Monomial m2(e2);
+  Monomial m3(e3);
 
-  Binomial b1(&m1, &m2);
-  Binomial b2(&m2, &m3);
+  Binomial b1(m1, m2);
+  Binomial b2(m2, m3);
   vector<Binomial> edges;
   edges.push_back(b1);
   edges.push_back(b2);
 
-  cout << inSameComponent (m1, m2, edges);
-  cout << inSameComponent (m1, m3, edges);
+  assert (!inSameComponent (m1, m2, edges));
+  assert (!inSameComponent (m1, m3, edges));
+}
 
+int main(){
+  t1();
   return 0;
 }
