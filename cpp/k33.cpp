6b7b4f7e2b10e4404769a93b7c780d91204beb85
Thomas Kahle <tomka@gentoo.org>
2012-04-30 00:09:48 +0200

    k33 is up and running

--- cpp/k33.cpp
+++ cpp/k33.cpp
@@ -28,15 +28,16 @@ int main() {
 
   // Examine one fiber to see how it is going.
   // Take some degree six markov move
-  // Binomial b((*markov)[16211]);
-  // cout << "Degree of move: " <<b.degree() << endl;
+  Binomial b((*markov)[16211]);
+  cout << "Degree of move: " <<b.degree() << endl;
 
-  // Monomial x2("2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
-  // Monomial m1(*(b.head->multiply(x2)));
-  // Monomial m2(*(b.tail->multiply(x2)));
-  // Binomial toTest(m1,m2);
-  // cout << toTest.head->toString() << endl;
-  // cout << toTest.tail->toString() << endl;
+  Monomial x2("1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+  Monomial m1(*(b.head->multiply(x2)));
+  Monomial m2(*(b.tail->multiply(x2)));
+  Binomial toTest(m1,m2);
+  cout << toTest.head->toString() << endl;
+  cout << toTest.tail->toString() << endl;
+  cout << inSameComponent (m1, m2, *quadrics);
   
   return 0;
 }
