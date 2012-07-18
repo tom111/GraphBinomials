373486e4ac5cf59c787c5e600f3615b257419be5
Thomas Kahle <tomka@gentoo.org>
2012-07-18 11:02:03 +0200

    run all tests

--- cpp/tests.cpp
+++ cpp/tests.cpp
@@ -237,18 +237,18 @@ void t12(){
 }
 
 int main(){
-  // t1();
-  // t2();
-  // t3();
-  // t4();
-  // t5();
-  // t6(); // approximately seven seconds
-  // t6_2(); // long ...
-  // t7();
-  // t8();
-  // t9();
-  // t10();
-  // t11();
+  t1();
+  t2();
+  t3();
+  t4();
+  t5();
+  t6(); // approximately seven seconds
+  t6_2(); // long ...
+  t7();
+  t8();
+  t9();
+  t10();
+  t11();
   t12();
   return 0;
 }
