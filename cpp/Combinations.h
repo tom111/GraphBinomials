64ce662a699cf1c5b63d4041e0219ccb0895687f
Thomas Kahle <thomas.kahle@jpberlin.de>
2014-07-11 15:51:38 +0200

    Improved version of Combinations.h.
    This fixes double free crashes in certain enumerations of pure O
    sequences in two variables.

--- cpp/Combinations.h
+++ cpp/Combinations.h
@@ -1,5 +1,3 @@
-// GPL Licenced code downloaded from Koders.com
-
 #ifndef COMBINATIONS_H
 #define COMBINATIONS_H
 #include <vector>
@@ -8,169 +6,206 @@
 // a class to iterate through m combinations out of n
 class Combinations
 {
- public:
- Combinations(int m, int n) : m_(m), n_(n), done_(0)
-    {
-      if (m_ <= n_)
-	{
-	  combination_.resize(m_);
-	  for (int i = 0; i < m_; i++) combination_[i] = i;
-	}
-    }
-  
-  int next()
-  {
-    if (done_) return !done_;
-    int pos = m_ - 1;
-    combination_[pos]++;
-    while (combination_[pos] > (n_ - m_ + pos) && !done_)
+   public:
+      Combinations(size_t m, size_t n) : m_(m), n_(n), done_(false)
       {
-	--pos;
-	combination_[pos]++;
-	for (int i = pos + 1; i < m_; i++)
-	  combination_[i] = combination_[i - 1] + 1;
-	if (combination_[0] > n_ - m_) done_ = 1;
+         if (m_ > n)
+         {
+            throw std::string("Combinations(m, n) : m must be less than or equal to n");
+         }
+         if (m_ == 0)
+         {
+            throw std::string("Combinations(m, n) : m must be greater than zero");
+         }
+         if (n_ == 0)
+         {
+            throw std::string("Combinations(m, n) : n must be greater than zero");
+         }
+         if (m_ <= n_)
+         {
+            combination_.resize(m_);
+            for (size_t i = 0; i < m_; i++) combination_[i] = i;
+         }
+         if (m_ == n_)
+         {
+            done_ = true;
+         }
       }
-    return !done_;
-  }
 
-  int operator() (int i) const
-  {
-    if (i < m_) return combination_[i];
-    return -1;
-  }
-  
-  unsigned int size() const
-  {
-    return combination_.size();
-  }
-  
-  int done() const
-  {
-    return done_;
-  }
-  
-  void display() const
-  {
-    for (int i = 0; i < m_; i++)
+      bool next()
       {
-	std::cout << combination_[i] << " ";
+         if (done_) return !done_;
+         size_t pos = m_ - 1;
+         combination_[pos]++;
+         while (!done_ && combination_[pos] > (n_ - m_ + pos))
+         {
+            if (pos > 0)
+            {
+                --pos;
+                combination_[pos]++;
+                for (size_t i = pos + 1; i < m_; i++)
+                {
+                combination_[i] = combination_[i - 1] + 1;
+                }
+                if (combination_[0] > n_ - m_) 
+                {
+                    done_ = true;
+                }
+            }
+            else
+            {
+                done_ = true;
+            }
+         }
+         return !done_;
       }
-    std::cout << std::endl;
-  }
-  
- private:
-  int m_;
-  int n_;
-  int done_;
-  std::vector<int> combination_;
+
+      size_t operator() (size_t i) const
+      {
+         if (i >= combination_.size())
+         {
+            throw std::string("Combination::operator() : index out of range");
+         }
+         return combination_[i];
+      }
+
+      size_t size() const
+      {
+         return combination_.size();
+      }
+
+      bool done() const
+      {
+         return done_;
+      }
+
+      void display() const
+      {
+         for (size_t i = 0; i < m_; i++)
+         {
+            std::cout << combination_[i] << " ";
+         }
+         std::cout << std::endl;
+      }
+
+   private:
+      size_t m_;
+      size_t n_;
+      bool done_;
+      std::vector<size_t> combination_;
 };
 
 class KnuthCombinations
 {
-  // Based on Algorithm 7.2.1.2T from "7.2.1.3 GENERATING ALL COMBINATIONS" of TAOCP (Fascicle 3A)
- public:
- KnuthCombinations(int t, int n) : t_(t), n_(n), done_(0)
-    {
-      if (t_ < n_)
-	{
-	  c_.resize(t_ + 2);
-	  // Step T1. [Initialize.]
-	  // Note: Knuth has 1 <= j <= t
-	  for (int j = 1; j <= t_; j++) c(j) = j - 1;
-	  c(t_ + 1) = n_;
-	  c(t_ + 2) = 0;
-	  j_ = t_;
-	}
-    }
-  
- private:
-  void increase_c_j(int x)
-  {
-    // Step T6. [Increase c_j.]
-    c(j_) = x;
-    --j_;
-  }
-  
- public:
-  bool next()
-  {
-    if (done_) return !done_;
-    // Step T2. [Visit.]
-    // Note: we assume any "visiting" has already been done at this point
-    if (j_ > 0)
+      // Based on Algorithm 7.2.1.2T from "7.2.1.3 GENERATING ALL COMBINATIONS" of TAOCP (Fascicle 3A)
+   public:
+      KnuthCombinations(size_t t, size_t n) : t_(t), n_(n), done_(false)
       {
-	increase_c_j(j_);
+         if (t_ > n)
+         {
+            throw std::string("KnuthCombinations(t, n) : t must be less than or equal to n");
+         }
+         if (t_ == 0)
+         {
+            throw std::string("KnuthCombinations(t, n) : t must be greater than zero");
+         }
+         if (n_ == 0)
+         {
+            throw std::string("KnuthCombinations(t, n) : n must be greater than zero");
+         }
+         c_.resize(t_ + 2);
+         // Step T1. [Initialize.]
+         // Note: Knuth has 1 <= j <= t, but I am using 0 <= j < t
+         for (size_t j = 0; j < t_; j++) 
+         {
+             c_[j] = j;
+         }
+         c_[t_] = n_;
+         c_[t_ + 1] = 0;
+         j_ = t_;
+         if (t_ == n_)
+         {
+            done_ = true;
+         }
       }
-    else
+
+   public:
+      bool next()
       {
-	// Step T3. [Easy case?]
-	if (c(1) + 1 < c(2))
-	  {
-	    ++c(1);
-	  }
-	else
-	  {
-	    j_ = 2;
-	    // Step T4. [Find j.]
-	    int x = 0;
-#if 1
-	    bool done = false;
-	    while (!done)
-	      {
-		c(j_ - 1) = j_ - 2;
-		x = c(j_) + 1;
-		if (x == c(j_ + 1)) ++j_;
-		else done = true;
-	      }
-#else
-	    c(j_ - 1) = j_ - 2;
-	    x = c(j_) + 1;
-	    while (x == c(j_ + 1)) ++j_;
-#endif
-	    // Step T5. [Done?]
-	    if (j_ > t_) done_ = true;
-	    increase_c_j(x);
-	  }
+         if (done_) 
+         {
+            return !done_;
+         }
+         // Step T2. [Visit.]
+         // Note: we assume any "visiting" has already been done at this point
+         if (j_ > 0)
+         {
+            c_[j_ - 1] = j_;
+            --j_;
+         }
+         else
+         {
+            // Step T3. [Easy case?]
+            if (c_[0] + 1 < c_[1])
+            {
+               ++c_[0];
+            }
+            else
+            {
+               j_ = 2;
+               // Step T4. [Find j.]
+               size_t x = 0;
+               bool done = false;
+               while (!done)
+               {
+                  c_[j_ - 2] = j_ - 2;
+                  x = c_[j_ - 1] + 1;
+                  if (x == c_[j_]) ++j_;
+                  else done = true;
+               }
+               // Step T5. [Done?]
+               if (j_ > t_) done_ = true;
+               c_[j_ - 1] = x;
+               --j_;
+            }
+         }
+         return !done_;
       }
-    return !done_;
-  }
-  
-  int operator() (int i) const
-  {
-    if (i < t_) return c_[i];
-    return -1;
-  }
-  
-  int size() const
-  {
-    return (c_.size() - 2);
-  }
-  
-  int done() const
-  {
-    return done_;
-  }
-  
-  void display() const
-  {
-    for (int i = 0; i < t_; i++)
+
+      size_t operator() (size_t i) const
       {
-	std::cout << c_[i] << " ";
+         if (i >= t_)
+         {
+            throw std::string("KnuthCombination::operator() : index out of range");
+         }
+         return c_[i];
       }
-    std::cout << std::endl;
-  }
-  
- private:
-  int t_;
-  int n_;
-  int j_;
-  int done_;
-  std::vector<int> c_;
-  int& c(int j)
-  {
-    return c_[j - 1];
-  }
+
+      size_t size() const
+      {
+         return (c_.size() - 2);
+      }
+
+      bool done() const
+      {
+         return done_;
+      }
+
+      void display() const
+      {
+         for (size_t i = 0; i < t_; i++)
+         {
+            std::cout << c_[i] << " ";
+         }
+         std::cout << std::endl;
+      }
+
+   private:
+      size_t t_;
+      size_t n_;
+      size_t j_;
+      bool done_;
+      std::vector<size_t> c_;
 };
 
 #endif
